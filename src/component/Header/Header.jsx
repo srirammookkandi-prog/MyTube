6ceb13f455b1478d3443675eeeff2d0d9d1473ca
srirammookkandi-prog <sriram.mookkandi@gmail.com>
2026-08-26 08:22:44 +0530

    Header elements added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { MENU_LOGO } from "../../utility/constants"
+import logo from "../../assest/logo.png"
+import { UserRound } from "lucide-react";
 
 const Header = () => {
     return (
-        <div>
+        <div className="flex">
+            <div className="flex justify-between">
+                <img className="w-8" alt="Menu Logo" src={MENU_LOGO} />
+                <img className="w-40" alt="App Logo" src={logo} />
+            </div>
+            <div>
+                <input placeholder="search" />
+                <span>Search</span>
+            </div>
+            <div>
+                <UserRound />
+            </div>
 
         </div>
     )

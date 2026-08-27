8d44542813d11f76abd8a5f2845b27a339a9b273
srirammookkandi-prog <sriram.mookkandi@gmail.com>
2026-08-27 06:59:07 +0530

    Header created

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,17 +4,17 @@ import { UserRound } from "lucide-react";
 
 const Header = () => {
     return (
-        <div className="flex">
+        <div className="flex justify-between m-4 shadow-lg">
             <div className="flex justify-between">
-                <img className="w-8" alt="Menu Logo" src={MENU_LOGO} />
-                <img className="w-40" alt="App Logo" src={logo} />
+                <img className="h-6 mx-2 mt-4" alt="Menu Logo" src={MENU_LOGO} />
+                <img className="w-28" alt="App Logo" src={logo} />
             </div>
-            <div>
-                <input placeholder="search" />
-                <span>Search</span>
+            <div className="w-1/2 m-2">
+                <input className="border border-black rounded-l-full w-1/2 py-1 px-4" placeholder="search" />
+                <button className="rounded-r-full border py-1 px-4 border-black">Search</button>
             </div>
-            <div>
-                <UserRound />
+            <div className="m-2">
+                <UserRound className="m-2" />
             </div>
 
         </div>

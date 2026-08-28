10fd3a1ba75cc19a20e482a0ff92809c83fc4e86
srirammookkandi-prog <sriram.mookkandi@gmail.com>
2026-08-28 23:44:13 +0530

    Video container completed

---
+++ src/component/Body/MainContainer/VideoContainer/VideoCard.jsx
@@ -0,0 +1,19 @@
+
+const VideoCard = ({ info }) => {
+    console.log(info);
+    const { snippet, statistics } = info;
+    const { thumbnails, channelTitle, title } = snippet;
+    return (
+        <div className="p-2 m-2 w-72 shadow-lg">
+            <img className="rounded-lg" alt='thumbnail' src={thumbnails.medium.url} />
+            <ul>
+                <li className="font-bold py-2">{title}</li>
+                <li className="font-semibold">{channelTitle}</li>
+                <li>{statistics.viewCount} views</li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default VideoCard
--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,1 +1,6 @@
 export const MENU_LOGO = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/250px-Hamburger_icon.svg.png?utm_source=en.wikipedia.org&utm_campaign=parser&utm_content=thumbnail';
+
+const API_KEY = "AIzaSyDXUwrk8lLaQvD4hw6B9jYL693wdLZGl1A";
+
+export const YouTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY;
+

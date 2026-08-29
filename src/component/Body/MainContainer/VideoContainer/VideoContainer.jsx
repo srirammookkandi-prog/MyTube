34fde559d481e316594f67aa6d5365942d9aa534
SRIRAM MOOKKANDI <sriram.mookkandi@gmail.com>
2026-08-29 20:55:56 +0530

    Watch Page video created (#5)

--- src/component/Body/MainContainer/VideoContainer/VideoContainer.jsx
+++ src/component/Body/MainContainer/VideoContainer/VideoContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YouTUBE_VIDEO_API } from '../../../../utility/constants';
 import VideoCard from './VideoCard';
+import { Link } from 'react-router-dom';
 
 const VideoContainer = () => {
     const [video, setVideo] = useState([]);
@@ -13,7 +14,7 @@ const VideoContainer = () => {
 
     return (video &&
         <div className='flex flex-wrap justify-between'>
-            {video.map(video => <VideoCard info={video} />)}
+            {video.map(video => <Link key={video.id} to={"/watch?v=" + video.id}><VideoCard info={video} /></Link>)}
         </div>
     )
 }
---
+++ src/component/WatchPage/WatchPage.jsx
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux"
+import { Closesidebar } from "../../utility/store/slice/toggleslidebar";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+
+const WatchPage = () => {
+    const [searchParams] = useSearchParams();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(Closesidebar());
+    }, [])
+
+    return (
+        <div className="m-4">
+            <iframe width="1200"
+                height="600"
+                src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen>
+
+            </iframe>
+        </div >
+    )
+}
+
+export default WatchPage
